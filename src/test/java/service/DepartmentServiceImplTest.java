9926d0cbab1d292aa8069ff8d4ebfff250315d83
Ramil-Karipov <omil8@yandex.ru>
2024-03-09 12:28:04 +0500

    Final2

--- src/test/java/service/DepartmentServiceImplTest.java
+++ src/test/java/service/DepartmentServiceImplTest.java
@@ -1,67 +1,62 @@
 package service;
 
 import model.Employee;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static service.TestUtils.getEmployee;
+import static org.mockito.Mockito.when;
+
 
 @ExtendWith(MockitoExtension.class)
 class DepartmentServiceImplTest {
 
     @Mock
-    private EmployeeService employeeService;
+    EmployeeService employeeService;
 
     @InjectMocks
-    private DepartmentServiceImpl departmentService;
+    DepartmentServiceImpl departmentService;
 
-    private final static List<Employee> EMPLOYEES = Arrays.asList(
-            getEmployee("TEST1", 10, 1),
-            getEmployee("TEST2", 20, 2),
-            getEmployee("TEST3", 30, 3)
+    @BeforeEach
+    void init() {
+        when(employeeService.getAllEmployees()).thenReturn(
+                List.of(
+                        new Employee("TEST1", 10, 1),
+                        new Employee("TEST2", 20, 2),
+                        new Employee("TEST3", 30, 3)
+                )
 
-    );
+        );
+    }
 
     @Test
     void getEmployeesByDepartmentIdTest() {
-        Mockito.when(employeeService.getAllEmployees()).thenReturn(EMPLOYEES);
-
-        assertEquals(1, departmentService.getEmployeesByDepartmentId(1).size());
+        assertEquals(1, departmentService.getEmployeesByDepartmentId(10).size());
     }
 
     @Test
     void getSalarySumByDepartmentIdTest() {
-        Mockito.when(employeeService.getAllEmployees()).thenReturn(EMPLOYEES);
-
-        assertEquals(20, departmentService.getSalarySumByDepartmentId(2));
+        assertEquals(2, departmentService.getSalarySumByDepartmentId(20));
     }
 
     @Test
     void getMaxSalaryByDepartmentId() {
-        Mockito.when(employeeService.getAllEmployees()).thenReturn(EMPLOYEES);
-
-        assertEquals(30, departmentService.getMaxSalaryByDepartmentId(3));
+        assertEquals(3, departmentService.getMaxSalaryByDepartmentId(30));
     }
 
     @Test
     void getMinSalaryByDepartmentId() {
-        Mockito.when(employeeService.getAllEmployees()).thenReturn(EMPLOYEES);
-
-        assertEquals(30, departmentService.getMinSalaryByDepartmentId(3));
+        assertEquals(3, departmentService.getMinSalaryByDepartmentId(30));
     }
 
     @Test
     void getAllEmployeesByDepartment() {
-        Mockito.when(employeeService.getAllEmployees()).thenReturn(EMPLOYEES);
-
         assertEquals(3, departmentService.getAllEmployeesByDepartment().size());
     }
-}
+    }
